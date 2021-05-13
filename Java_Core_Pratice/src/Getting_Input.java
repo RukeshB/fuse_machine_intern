48dcf41746b9094c7a04b9bfe58569e22913d9b5
RukeshB <rukeshbasukala@gmail.com>
2021-05-13 14:37:41 +0545

    loop and input pratice

---
+++ Java_Core_Pratice/src/Getting_Input.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Getting_Input {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Enter your Name: ");
+		
+		String name = sc.nextLine();
+		
+		System.out.print("Enter your age: ");
+		int age = sc.nextInt();
+		
+		System.out.print("Enter your salary: ");
+		double salary = sc.nextDouble();
+		
+		System.out.println("Name: "+ name);
+		System.out.println("age: "+ age);
+		System.out.println("salary: "+ salary);
+	}
+
+}
---
+++ Java_Core_Pratice/src/loop.java
@@ -0,0 +1,31 @@
+
+public class loop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//for loop
+		System.out.println("For loop");
+		for(int i=0;i<5;i++)
+		{
+			System.out.println(i);
+		}
+		
+		//while loop
+		int j = 0;
+		System.out.println("while loop");
+		while(j<5)
+		{
+			System.out.println(j);
+			j++;
+		}
+		
+		//do while loop
+		int k = 0;
+		System.out.println("do while loop");
+		do {
+			System.out.println(k);
+			k++;
+		}while(k<5);
+	}
+
+}
