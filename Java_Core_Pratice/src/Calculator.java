633edfb2fcc7ca2cb23d274cc594f8a83062bac6
RukeshB <rukeshbasukala@gmail.com>
2021-05-13 23:19:15 +0545

    class and object used in calculator

--- Java_Core_Pratice/src/Calculator.java
+++ Java_Core_Pratice/src/Calculator.java
@@ -1,52 +1,7 @@
 
 import java.util.Scanner;
 
-public class calculator {
-
-	public double add(double[] num)
-	{
-		double sum = 0;
-		for(int i=0;i<num.length;i++)
-		{
-			sum += num[i];
-		}
-		return sum;
-		//System.out.println(a + " + " + b +" = "+(a+b));
-	}
-	
-	public double subtract(double[] num)
-	{
-		double sub = 0;
-		for(int i=0;i<num.length;i++)
-		{
-			sub -= num[i];
-		}
-		return sub;
-		//System.out.println(a + " - " + b +" = "+(a-b));
-	}
-	
-	public double Divide(double[] num)
-	{
-		double div = num[0];
-		for(int i=1;i<num.length;i++)
-		{
-			div = div/num[i];
-		}
-		return div;
-		//System.out.println(a + " / " + b +" = "+(a/b));
-	}
-	
-	public double Multiply(double[] num)
-	{
-		double mul = 1;
-		for(int i=0;i<num.length;i++)
-		{
-			mul *= num[i];
-		}
-		return mul;
-		//System.out.println(a + " * " + b +" = "+(a*b));
-	}
-	
+public class Calculator {
 	public static void clearScreen() {  
 		System.out.print("\033[H\033[2J");  
 	    System.out.flush();  
@@ -54,7 +9,7 @@ public class calculator {
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		calculator cal = new calculator();
+		Calculator_operation cal = new Calculator_operation();
 		
 		Scanner input = new Scanner(System.in);
 		
---
+++ Java_Core_Pratice/src/Calculator_operation.java
@@ -0,0 +1,46 @@
+
+public class Calculator_operation {
+	public double add(double[] num)
+	{
+		double sum = 0;
+		for(int i=0;i<num.length;i++)
+		{
+			sum += num[i];
+		}
+		return sum;
+		//System.out.println(a + " + " + b +" = "+(a+b));
+	}
+	
+	public double subtract(double[] num)
+	{
+		double sub = num[0];
+		for(int i=1;i<num.length;i++)
+		{
+			sub -= num[i];
+		}
+		return sub;
+		//System.out.println(a + " - " + b +" = "+(a-b));
+	}
+	
+	public double Divide(double[] num)
+	{
+		double div = num[0];
+		for(int i=1;i<num.length;i++)
+		{
+			div = div/num[i];
+		}
+		return div;
+		//System.out.println(a + " / " + b +" = "+(a/b));
+	}
+	
+	public double Multiply(double[] num)
+	{
+		double mul = 1;
+		for(int i=0;i<num.length;i++)
+		{
+			mul *= num[i];
+		}
+		return mul;
+		//System.out.println(a + " * " + b +" = "+(a*b));
+	}
+}
