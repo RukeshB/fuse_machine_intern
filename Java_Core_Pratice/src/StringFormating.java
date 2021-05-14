c7e57cf0f839297149e5910761046b68df255fef
RukeshB <rukeshbasukala@gmail.com>
2021-05-14 16:58:41 +0545

    pratice String Formating

---
+++ Java_Core_Pratice/src/StringFormating.java
@@ -0,0 +1,12 @@
+
+public class StringFormating {
+
+	public static void main(String[] args) {
+		System.out.printf("FirstName\t\tLastName\t\tClass\n");
+		System.out.printf("%-10s\t\t%-10s\t\t%-2d\n","Ram","Basu",9);
+		System.out.printf("%-10s\t\t%-10s\t\t%-2d\n","Shyam","Thapa",10);
+		System.out.printf("%-10s\t\t%-10s\t\t%-2d\n","Gita","Rai",10);
+		System.out.printf("%-10s\t\t%-10s\t\t%-2d\n","kamless","swual",8);
+	}
+
+}
