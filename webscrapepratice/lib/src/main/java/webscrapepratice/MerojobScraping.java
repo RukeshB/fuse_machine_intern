3f0e7d54bb33e12fe8c3970c38c0c8f2869db057
RukeshB <rukeshbasukala@gmail.com>
2021-05-25 13:42:29 +0545

    storing scraped data in hashmap

--- webscrapepratice/lib/src/main/java/webscrapepratice/MerojobScraping.java
+++ webscrapepratice/lib/src/main/java/webscrapepratice/MerojobScraping.java
@@ -1,5 +1,8 @@
 package webscrapepratice;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
@@ -7,15 +10,20 @@ import org.jsoup.select.Elements;
 
 public class MerojobScraping {
 
+	public static ArrayList<String> jobs = new ArrayList<String>();
+	public static HashMap<Integer, ArrayList<String>> jobdetails = new HashMap<Integer, ArrayList<String>>();
+	
 	public static void main(String[] args) {
+		int jobnumber = 0;
 		final String url = "https://merojob.com";
 		try
 		{
 			Document doc = Jsoup.connect(url).get();
+			String link= "";
 			for(Element element : doc.select("div.job-card"))
 			{
 				String alldetail = element.select("h2.h6.mb-1").text();
-				String link = element.select("h2.h6.mb-1 a[href]").attr("href");
+				link = element.select("h2.h6.mb-1 a[href]").attr("href");
 				
 				if(alldetail != "")
 				{
@@ -38,12 +46,13 @@ public class MerojobScraping {
 					
 					//getting detail of job 
 					final String indjoblink = url+link;
-					JobDetail(indjoblink);
-					//System.out.println(company + " => " + job +" => " + url+link);
-					//System.out.print("\n");
+					jobnumber++;
+					JobDetail(indjoblink,jobnumber);
+					jobs.clear();
 				}
 				
 			}
+			System.out.println(jobdetails);
 			System.out.println("sucessful");
 		}
 		catch(Exception ex)
@@ -52,7 +61,7 @@ public class MerojobScraping {
 		}
 	}
 	
-	public static void JobDetail(String link)
+	public static void JobDetail(String link,int jobnumber)
 	{
 		
 		try
@@ -64,26 +73,40 @@ public class MerojobScraping {
 			String description = elements.select("div#short-description").text();
 			String jobTitle = elements.select("div.card div.card-header div.float-left h1.h4.mb-0.text-primary").text();
 			
-			System.out.println("company Name => "+companyName);
-			System.out.println("job Catagory => "+jobCatagory);
-			System.out.println("description => "+description);
-			System.out.println("jobTitle => "+jobTitle);
+			jobs.add(link);
+			jobs.add(companyName);
+			jobs.add(jobCatagory);
+			//jobs.add(description);
+			jobs.add(jobTitle);
+			//System.out.println("company Name => "+companyName);
+			//System.out.println("job Catagory => "+jobCatagory);
+			//System.out.println("description => "+description);
+			//System.out.println("jobTitle => "+jobTitle);
 			
-			System.out.println("");
+			//System.out.println("");
 			for(Element element : elements.select("div.card-body div.card-group"))
 			{
 				String header = element.select("div.card-header.p-2 h3.mb-1.h6 strong").text();
-				System.out.println(header);
+				//System.out.println(header);
 				
 				for(Element subelement: element.select("table.table.table-hover.table-no-border.m-0 tbody tr"))
 				{
 					String information = subelement.text();
-					System.out.println(information);
+					
+					String[] details = information.split(" : ");
+					jobs.add(details[1]);
+					
+					//System.out.println(information);
 				}
-				System.out.println("");
+				//System.out.println("");
 			}
 			
+			System.out.println(jobs);
+			jobdetails.put(jobnumber, jobs);
+			
 			System.out.print("\n\n");
+			
+			
 		}
 		catch(Exception ex)
 		{
