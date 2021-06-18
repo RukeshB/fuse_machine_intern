cb08ca786be05f3e2b0a505af1db035e71564187
RukeshB <rukeshbasukala@gmail.com>
2021-06-18 17:22:49 +0545

    basic auth pratice

---
+++ JWTAuthPratice/src/main/java/com/jwtauth/pratice/JwtAuthPraticeApplication.java
@@ -0,0 +1,13 @@
+package com.jwtauth.pratice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtAuthPraticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtAuthPraticeApplication.class, args);
+	}
+
+}
---
+++ JWTAuthPratice/src/test/java/com/jwtauth/pratice/JwtAuthPraticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jwtauth.pratice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtAuthPraticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
