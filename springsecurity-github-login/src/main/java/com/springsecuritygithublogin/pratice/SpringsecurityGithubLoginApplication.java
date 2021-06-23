a449c316aade76c8676e31478694944e5220c682
RukeshB <rukeshbasukala@gmail.com>
2021-06-23 15:02:19 +0545

    pratice spring security github login

---
+++ springsecurity-github-login/src/main/java/com/springsecuritygithublogin/pratice/SpringsecurityGithubLoginApplication.java
@@ -0,0 +1,15 @@
+package com.springsecuritygithublogin.pratice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+
+@SpringBootApplication
+
+public class SpringsecurityGithubLoginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringsecurityGithubLoginApplication.class, args);
+	}
+
+}
