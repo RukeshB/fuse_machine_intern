beab16fba23bdaf8cfada79fae1388e71c79928a
RukeshB <rukeshbasukala@gmail.com>
2021-06-28 17:22:22 +0545

    simple junit program

---
+++ simple_junit_project/src/simple_junit_project/JunitTest1.java
@@ -0,0 +1,22 @@
+package simple_junit_project;
+
+import org.junit.jupiter.api.Test;
+
+public class JunitTest1 {
+
+	@Test
+	public void test1()
+	{
+		System.out.println("test1");
+	}
+	
+	@Test
+	public void test2()
+	{
+		System.out.println("test2");
+	}
+	
+	public static void main(String[] args) {
+		
+	}
+}
