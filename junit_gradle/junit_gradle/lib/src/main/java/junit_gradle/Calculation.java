16f0c144555729d10561c61a8faf28b2e9f297ec
RukeshB <rukeshbasukala@gmail.com>
2021-06-30 13:33:50 +0545

    pratice junit

--- junit_gradle/junit_gradle/lib/src/main/java/junit_gradle/Calculation.java
+++ junit_gradle/junit_gradle/lib/src/main/java/junit_gradle/Calculation.java
@@ -2,8 +2,23 @@ package junit_gradle;
 
 public class Calculation {
 
-	public int addnumber(int a,int b)
+	public int add(int a,int b)
 	{
 		return a+b;
+	}
+	
+	public double divide(int a, int b)
+	{
+		return a/b;
+	}
+	
+	public int multiply (int a , int b)
+	{
+		return a*b;
+	}
+	
+	public double areaOfCircle(double radius)
+	{
+		return Math.PI * Math.pow(radius, 2);
 	}
 }
--- junit_gradle/junit_gradle/lib/src/test/java/junit_gradle/CalculationTest.java
+++ junit_gradle/junit_gradle/lib/src/test/java/junit_gradle/CalculationTest.java
@@ -2,23 +2,71 @@ package junit_gradle;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInfo;
+import org.junit.jupiter.api.TestReporter;
 
 class CalculationTest {
-	private Calculation cal = new Calculation();
+	private Calculation cal;
+	
+	@BeforeEach
+	void init(TestInfo testInfo,TestReporter testReporter)
+	{
+		cal = new Calculation();
+		testReporter.publishEntry("Running "+testInfo.getTestMethod().get()+" Method with Tag "+testInfo.getTags());
+	}
+	
 	@Test
-	void test() {
+	@DisplayName("Testing add method")
+	@Tag("basicMath")
+	void testAdd() {
 		int expected = 2;
-		int actual = cal.addnumber(1, 1);
+		int actual = cal.add(1, 1);
 		
-		assertEquals(expected, actual);
+		assertEquals(expected, actual,"add methode add two number");
 	}
 
 	@Test
-	void test2() {
-		int expected = 3;
-		int actual = cal.addnumber(1, 1);
+	@Tag("basicMath")
+	void testDivide()
+	{
+		//fail test
+		//assertThrows(NullPointerException.class,()-> cal.divide(1, 0));
 		
-		assertEquals(expected, actual);
+		//sccuess test
+		assertThrows(ArithmeticException.class,()-> cal.divide(1, 0));
+	}
+	
+	@Test
+	@DisplayName("test multiply method")
+	@Tag("basicMath")
+	void testMultiply()
+	{
+		assertAll(
+				()->assertEquals(4, cal.multiply(2, 2)),
+				()->assertEquals(0, cal.multiply(0, 2)),
+				()->assertEquals(-10, cal.multiply(2, -5))
+				);
+	}
+	
+	@Test
+	@Tag("geometric")
+	void testAreaOfCircle() 
+	{
+		double expected = 314.1592653589793;
+		double actual = cal.areaOfCircle(10);
+		
+		assertEquals(expected, actual,"return area of circle");
+	}
+	
+	@Test
+	@Disabled
+	void testDisable()
+	{
+		fail("this test is not complete");
 	}
 }
